a467313680cea15ad164c85c476e20a94fda8a34
Carsten Griesheimer <sun_od@hotmail.com>
2011-12-06 18:28:46 +0100

    Mareks Fehler ausbaden

---
+++ src/Engine.java
@@ -0,0 +1,123 @@
+import lejos.nxt.Motor;
+import lejos.nxt.NXTRegulatedMotor;
+
+/**
+ * 
+ * Motor A: rechts
+ * Motor C: links
+ */
+public class Engine {
+	
+	private final NXTRegulatedMotor LEFT = Motor.C; 
+	private final NXTRegulatedMotor RIGHT = Motor.A;
+	
+	public final int MAX_SPEED = 900;
+	private boolean turning = false;
+	private int QUARTER_TURN = 100;
+	
+	public Engine() {
+		
+	}
+	
+	/**
+	 * Lege Geschindigkeit fest.
+	 * Die Geschwindigkeit wird dabei als Buchteil der Maximalgeschwindigkeit festgelegt.
+	 * @param v Geschwindigkeit in Prozent [0,100]
+	 */
+	private void setSpeed(int v) {
+		if (v < 0 && v > 100) throw new IllegalArgumentException();
+		
+		float motor_speed = MAX_SPEED * v / 100;
+		
+		LEFT.setSpeed(motor_speed);
+		RIGHT.setSpeed(motor_speed);	
+	}
+	
+	public void forward(int v) {
+		setSpeed(v);
+		resetTacho();
+		LEFT.forward();
+		RIGHT.forward();
+	}
+	
+	public void backward(int v) {
+		setSpeed(v);
+		resetTacho();
+		LEFT.backward();
+		RIGHT.backward();
+	}
+	
+	public void stop() {
+		setSpeed(0);
+		resetTacho();
+		LEFT.stop();
+		RIGHT.stop();
+	}
+	
+	public void turn() {
+		setSpeed(100);
+		resetTacho();
+		LEFT.forward();
+		RIGHT.backward();
+		turning = true;
+	}
+	
+	/**
+	 * Gibt an wieviel Prozent das linke Rad langsamer sein soll als das rechte.
+	 *
+	 * Bei 100% blockiert das linke Rad.
+	 * Bei 0% fährt der Roboter grade aus.
+	 * 
+	 * @param p stärke der Kurve [0, 100]
+	 */
+	public void bendLeft(int p) {
+		if (p < 0 && p > 100) throw new IllegalArgumentException();
+		resetTacho();
+		float left_speed = RIGHT.getSpeed() * p / 100f;
+		LEFT.setSpeed(left_speed);
+	}
+	
+	/**
+	 * Gibt an wieviel Prozent das rechte Rad langsamer sein soll als das linke.
+	 *
+	 * Bei 100% blockiert das rechte Rad.
+	 * Bei 0% fährt der Roboter grade aus.
+	 * 
+	 * @param p stärke der Kurve [0, 100]
+	 */
+	public void bendRight(int p) {
+		if (p < 0 && p > 100) throw new IllegalArgumentException();
+		resetTacho();
+		float right_speed = LEFT.getSpeed() * p / 100f;
+		RIGHT.setSpeed(right_speed);
+	}
+	
+	/**
+	 * @return true wenn der Roboter in Bewegung ist
+	 */
+	public boolean isMoving() {
+		return LEFT.isMoving() || RIGHT.isMoving();
+	}
+	
+	private void resetTacho() {
+		LEFT.resetTachoCount();
+		RIGHT.resetTachoCount();
+		turning = false;
+	}
+	
+	private void correctDirection() {
+		
+	}
+	
+	public void checkTurning() {
+		if (turning) {
+			int count = LEFT.getTachoCount() + RIGHT.getTachoCount();
+			if (count == QUARTER_TURN) stop();
+		}
+	}
+	
+	public void update() {
+		checkTurning();
+		if ( isMoving() ) correctDirection();
+	}
+}
