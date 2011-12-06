62c27207d87de17e9ad65e054418ee28babcdd25
Carsten Griesheimer <sun_od@hotmail.com>
2011-12-06 15:48:06 +0100

    Engin Klasse
    Grundlegende Bewegungs-Funktionen

--- src/Engine.java
+++ src/Engine.java
@@ -13,32 +13,38 @@ public class Engine {
 	}
 	
 	/**
-	 * Lege Geschindigkeit fest
+	 * Lege Geschindigkeit fest.
+	 * Die Geschwindigkeit wird dabei als Buchteil der Maximalgeschwindigkeit festgelegt.
 	 * @param v Geschwindigkeit in Prozent [-100,100]
 	 */
 	public void setSpeed(int v) {
+		float motor_speed = MAX_SPEED * v / 100;
+		if (motor_speed < 0) motor_speed *= -1;
 		
-	}
-	
-	public void forward(int v) {
-		
+		Motor.A.setSpeed(motor_speed);
+		Motor.B.setSpeed(motor_speed);	
 	}
 	
 	public void forward() {
-		speed = MAX_SPEED;
-		Motor.A.setSpeed(speed);
-		Motor.B.setSpeed(speed);
+		setSpeed(100);
+		Motor.A.forward();
+		Motor.B.forward();
+	}
+	
+	public void backward() {
+		setSpeed(-100);
+		Motor.A.backward();
+		Motor.B.backward();
 	}
 	
 	public void stop() {
-		Motor.A.setSpeed(0);
-		Motor.B.setSpeed(0);
+		setSpeed(0);
+		Motor.A.stop();
+		Motor.B.stop();
 	}
 	
-	public void turn(float drehung) {
-		Motor.A.setSpeed(MAX_SPEED);
-		Motor.B.setSpeed(MAX_SPEED);
-		
+	public void turn() {
+		setSpeed(100);
 		Motor.A.forward();
 		Motor.B.backward();
 	}
