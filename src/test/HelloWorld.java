b7c1884c39e397eeca37f8a36bd459348c9d4fd0
Carsten Griesheimer <sun_od@hotmail.com>
2011-12-08 19:10:03 +0100

    .

---
+++ src/test/HelloWorld.java
@@ -0,0 +1,25 @@
+package test;
+import lejos.nxt.*;
+import basis.RobotState;
+
+/**
+ * $Id: HelloWorld.java 1587 2008-05-02 17:19:41Z lgriffiths $
+ * 
+ * @author Lawrie Griffiths
+ * 
+ */
+public class HelloWorld
+{
+  public static void main (String[] aArg)
+  throws Exception
+  {
+     RobotState r = RobotState.getInstance();
+     //r.addBehaviour(new DriveForwardAndStopBehaviour());
+     //r.addBehaviour(new EngineTestBehaviour());
+     //r.addBehaviour(new LineFollowBehaviour());
+     r.driveForward(50);
+     while(true) {
+    	 r.update();
+     }
+  }
+}
