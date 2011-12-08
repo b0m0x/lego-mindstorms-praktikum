d1d1dcb82760670cec5257c413066d27bbdff8f1
Carsten Griesheimer <sun_od@hotmail.com>
2011-12-08 20:38:24 +0100

    zw

--- src/behaviour/LabyrinthBehaviour.java
+++ src/behaviour/LabyrinthBehaviour.java
@@ -1,5 +1,6 @@
 package behaviour;
 
+import lejos.nxt.Sound;
 import basis.RobotState;
 import basis.SensorArm.SensorArmPosition;
 import behaviour.RobotBehaviour;
@@ -7,19 +8,48 @@ import behaviour.RobotBehaviour;
 public class LabyrinthBehaviour implements RobotBehaviour {
 	private static final int WALL_DISTANCE = 10;
 	
+	enum pos { front, right };
+	
+	private pos akt_pos;
+	private pos next_pos;
+	
+	private RobotState state;
 	public void init(RobotState r) {
-		r.setSensorArmPosition(SensorArmPosition.POSITION_LABYRINTH);
-		r.forward(30);
+		state = r;
+		
+		r.setSensorArmPosition(SensorArmPosition.POSITION_FRONT);
+		r.halt();
+		akt_pos = pos.front;
 	}
 
 	public void update(RobotState r) {
-		int realWallDist = r.getUltraSonic();
+		//int realWallDist = r.getUltraSonic();
 		
+		switch (akt_pos) {
+		case front: front();
+		case right: right();
+		default: 
+			System.out.println("bababa");
+		}
+		
+		/*
+		r.setSensorArmPosition(SensorArmPosition.POSITION_FRONT);
+		r.isSensorArmMoving();
 		if (realWallDist > WALL_DISTANCE) { //entfernt, lenke nach rechts
 			r.bendLeft(30 * WALL_DISTANCE / realWallDist);
 		} else {
 			r.bendLeft(30 * WALL_DISTANCE / realWallDist);
 		}
+		*/
+	}
+	
+	private void front() {
+		int entfernung = state.getUltraSonic();
+		if (entfernung < 10) Sound.buzz();
+		//next_pos = pos.right;
+	}
+	
+	private void right() {
 		
 	}
 
--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -1,6 +1,7 @@
 package test;
 import lejos.nxt.*;
 import basis.RobotState;
+import behaviour.LabyrinthBehaviour;
 
 /**
  * $Id: HelloWorld.java 1587 2008-05-02 17:19:41Z lgriffiths $
@@ -13,13 +14,20 @@ public class HelloWorld
   public static void main (String[] aArg)
   throws Exception
   {
+	 Sound.buzz();
+	 Sound.buzz();
+	 Sound.buzz();
+  	 /*
      RobotState r = RobotState.getInstance();
      //r.addBehaviour(new DriveForwardAndStopBehaviour());
      //r.addBehaviour(new EngineTestBehaviour());
      //r.addBehaviour(new LineFollowBehaviour());
-     r.forward(50);
-     while(true) {
+     r.addBehaviour(new LabyrinthBehaviour());
+     r.init();
+     // r.forward(50);
+     while(!Button.ESCAPE.isPressed()) { 
     	 r.update();
      }
+     */
   }
 }
