3888de11d2389fea035b5096822f97f05ba61505
Marek Wehmer <marek.wehmer@googlemail.com>
2011-12-08 14:16:34 +0100

    added SensorArm controller

---
+++ src/basis/SensorArm.java
@@ -0,0 +1,37 @@
+package basis;
+
+import lejos.nxt.Motor;
+import lejos.nxt.NXTRegulatedMotor;
+
+public class SensorArm {
+	enum SensorPosition {
+		POSITION_LINE_FOLLOW,
+		POSITION_LABYRINTH,
+		POSITION_HILL
+	}
+	private final NXTRegulatedMotor SENSOR_MOTOR = Motor.B;
+	
+
+	public SensorArm() {
+		SENSOR_MOTOR.resetTachoCount();
+	}
+	
+	public void setPosition(SensorPosition p) {
+		int absAngle = getPositionAngle(p) - SENSOR_MOTOR.getTachoCount();
+		SENSOR_MOTOR.rotateTo(absAngle, true);
+	}
+	
+	
+	private int getPositionAngle(SensorPosition p) {
+		switch(p) {
+			case POSITION_LINE_FOLLOW:
+			case POSITION_HILL:
+				//TODO: INSERT CORRECT VALUE
+				return 0;
+			case POSITION_LABYRINTH:
+				return 120;
+			default: 
+				return 0;
+		}
+	}
+}
