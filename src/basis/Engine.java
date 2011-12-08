04be1b9bbdfe5b23823592df5263f4dfac2e8a1d
Carsten Griesheimer <sun_od@hotmail.com>
2011-12-08 11:50:27 +0100

    -_-

---
+++ src/basis/Engine.java
@@ -0,0 +1,169 @@
+package basis;
+import lejos.nxt.Motor;
+import lejos.nxt.NXTRegulatedMotor;
+
+/**
+ * 
+ * Motor A: rechts
+ * Motor C: links
+ */
+public class Engine {
+	
+	private final NXTRegulatedMotor LEFT = Motor.C; 
+	private final NXTRegulatedMotor RIGHT = Motor.A;
+
+	//private final int DISTANCE_PER_DEGREE = RADUMPFANG / 360; //mm
+	private double UEBERSETZUNG = 1.05;
+	
+	public final int MAX_SPEED = 900;
+	private boolean turning = false;
+	
+	//for distance count
+	private boolean driveMaxDist;
+	private int maxTachoCount;
+	
+	public Engine() {
+		
+	}
+	
+	/**
+	 * Lege Geschindigkeit fest.
+	 * Die Geschwindigkeit wird dabei als Buchteil der Maximalgeschwindigkeit festgelegt.
+	 * @param v Geschwindigkeit in Prozent [0,100]
+	 */
+	private void setSpeed(int v) {
+		if (v < 0 && v > 100) throw new IllegalArgumentException();
+		
+		float motor_speed = MAX_SPEED * v / 100;
+		
+		LEFT.setSpeed(motor_speed);
+		RIGHT.setSpeed(motor_speed);	
+	}
+	
+	public void backward(int v) {
+		setSpeed(v);
+		resetTacho();
+		LEFT.forward();
+		RIGHT.forward();
+	}
+	
+	public void forward(int v) {
+		setSpeed(v);
+		resetTacho();
+		LEFT.backward();
+		RIGHT.backward();
+	}
+	
+	public void stop() {
+		setSpeed(0);
+		resetTacho();
+		LEFT.stop();
+		RIGHT.stop();
+	}
+	
+	public void turnLeft(float degree) {
+		resetTacho();
+		setSpeed(30);
+		LEFT.rotate((int)(degree * UEBERSETZUNG * (-1)), true);
+		RIGHT.rotate((int)(degree * UEBERSETZUNG), true);
+		turning = true;
+	}
+	
+	public void setMaxDist(int tachoCount) {
+		maxTachoCount = tachoCount;
+		driveMaxDist = true;
+	}
+	
+	/**
+	 * Gibt an wieviel Prozent das linke Rad langsamer sein soll als das rechte.
+	 *
+	 * Bei 100% blockiert das linke Rad.
+	 * Bei 0% f�hrt der Roboter grade aus.
+	 * 
+	 * @param p st�rke der Kurve [0, 100]
+	 */
+	public void bendLeft(int p) {
+		if (p < 0 && p > 100) throw new IllegalArgumentException();
+		float right_speed = RIGHT.getSpeed();
+		float left_speed = right_speed * p / 100f;
+		
+		resetTacho();
+		RIGHT.setSpeed(right_speed);
+		LEFT.setSpeed(left_speed);
+		RIGHT.backward();
+		LEFT.backward();
+	}
+	
+	/**
+	 * Gibt an wieviel Prozent das rechte Rad langsamer sein soll als das linke.
+	 *
+	 * Bei 100% blockiert das rechte Rad.
+	 * Bei 0% f�hrt der Roboter grade aus.
+	 * 
+	 * @param p st�rke der Kurve [0, 100]
+	 */
+	public void bendRight(int p) {
+		if (p < 0 && p > 100) throw new IllegalArgumentException();
+		float left_speed = LEFT.getSpeed();
+		float right_speed = left_speed * p / 100f;
+		resetTacho();
+		RIGHT.setSpeed(right_speed);
+		LEFT.setSpeed(left_speed);
+		RIGHT.backward();
+		LEFT.backward();
+	}
+	
+	/**
+	 * @return true wenn der Roboter in Bewegung ist
+	 */
+	public boolean isMoving() {
+		return LEFT.isMoving() || RIGHT.isMoving();
+	}
+	
+	private void resetTacho() {
+		LEFT.resetTachoCount();
+		RIGHT.resetTachoCount();
+		turning = false;
+	}
+	
+	public void driveDistance(int distance, int speed, boolean flt) {
+		throw new UnsupportedOperationException();
+	/*
+		double realDistance = 0.0;
+		
+		resetTacho();
+		setSpeed(speed);
+		RIGHT.rotate((distance / DISTANCE_PER_DEGREE), true);
+		LEFT.rotate((distance / DISTANCE_PER_DEGREE));
+		
+		//�berpr�fe ob die zur�ckgelegte Distanz der Zieldistanz entspricht
+		while (realDistance <= distance) {
+			realDistance = Math.abs(RIGHT.getTachoCount()) * DISTANCE_PER_DEGREE;
+		}
+		
+		//bei boolean flt = true, wird der Motor nicht sofort gestoppt sondern dreht sich aus
+		if(flt) {
+			RIGHT.flt();
+			LEFT.flt();
+		} else {
+			stop();
+		}
+	*/
+	}
+	
+	public void checkTurning() {
+		if (turning && !isMoving()) turning = false;
+	}
+	
+	public void update() {
+		checkTurning();
+		if (driveMaxDist) {
+			System.out.println("" + LEFT.getTachoCount() + "/" + maxTachoCount);
+			if (Math.abs(LEFT.getTachoCount()) > maxTachoCount) {
+				stop();
+				driveMaxDist = false;
+			}
+		}
+		//if ( isMoving() );
+	}
+}
