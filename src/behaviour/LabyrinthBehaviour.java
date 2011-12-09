eeaf0d0002f886c1efb1f5790a035ddef9358a12
Carsten Griesheimer <sun_od@hotmail.com>
2011-12-09 10:48:11 +0100

    + Labyrinth und Helper-Klassen

--- src/behaviour/LabyrinthBehaviour.java
+++ src/behaviour/LabyrinthBehaviour.java
@@ -1,56 +1,88 @@
 package behaviour;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import lejos.nxt.Sound;
 import basis.RobotState;
 import basis.SensorArm.SensorArmPosition;
 import behaviour.RobotBehaviour;
+import helper.*;
 
 public class LabyrinthBehaviour implements RobotBehaviour {
-	private static final int WALL_DISTANCE = 10;
 	
-	enum pos { front, right };
+	//private static final int WALL_DISTANCE = 10;
+	private final int FRONT_DISTANZ = 30;
+	private final int RIGHT_DISTANZ = 15;
 	
+	enum pos {right, front};
 	private pos akt_pos;
-	private pos next_pos;
+	RobotState state;
+	Eieruhr uhr = new Eieruhr(2000);
+	Messwerte messwerte = new Messwerte();
 	
-	private RobotState state;
 	public void init(RobotState r) {
-		state = r;
-		
 		r.setSensorArmPosition(SensorArmPosition.POSITION_FRONT);
-		r.halt();
+		r.forward(30);
+		state = r;
 		akt_pos = pos.front;
+		uhr.reset();
 	}
-
+	
 	public void update(RobotState r) {
-		//int realWallDist = r.getUltraSonic();
-		
-		switch (akt_pos) {
-		case front: front();
-		case right: right();
-		default: 
-			System.out.println("bababa");
+	
+		if (!state.isSensorArmMoving()) {
+			int distanz = state.getUltraSonic();
+			messwerte.add(distanz);
+			
+			if (uhr.isFinished()) nextAction();
 		}
+		
 		
 		/*
-		r.setSensorArmPosition(SensorArmPosition.POSITION_FRONT);
-		r.isSensorArmMoving();
+		int realWallDist = r.getUltraSonic();
+		
 		if (realWallDist > WALL_DISTANCE) { //entfernt, lenke nach rechts
-			r.bendLeft(30 * WALL_DISTANCE / realWallDist);
+		r.bendLeft(30 * WALL_DISTANCE / realWallDist);
 		} else {
-			r.bendLeft(30 * WALL_DISTANCE / realWallDist);
+		r.bendLeft(30 * WALL_DISTANCE / realWallDist);
 		}
 		*/
+	
 	}
 	
-	private void front() {
-		int entfernung = state.getUltraSonic();
-		if (entfernung < 10) Sound.buzz();
-		//next_pos = pos.right;
-	}
-	
-	private void right() {
+	private void nextAction() {
+		int distanz = messwerte.getAverage();
+		messwerte.clear();
 		
+		switch(akt_pos) {
+			case front: front(distanz);
+			case right: right(distanz);
+		}
+	}
+	
+	public void front(int distanz) {
+		if (distanz > FRONT_DISTANZ) {
+			state.setSensorArmPosition(SensorArmPosition.POSITION_RIGHT);
+			akt_pos = pos.right;
+		} else {
+			Sound.beep();
+		}
+	}
+	
+	public void right(int distanz) {
+		if (distanz > RIGHT_DISTANZ) {
+			state.setSensorArmPosition(SensorArmPosition.POSITION_FRONT);
+			akt_pos = pos.front;
+		} else {
+			Sound.beep();
+		}
+	}
+
+	@Override
+	public boolean isNextLevel() {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -1,33 +1,17 @@
 package test;
-import lejos.nxt.*;
-import basis.RobotState;
-import behaviour.LabyrinthBehaviour;
 
-/**
- * $Id: HelloWorld.java 1587 2008-05-02 17:19:41Z lgriffiths $
- * 
- * @author Lawrie Griffiths
- * 
- */
-public class HelloWorld
-{
-  public static void main (String[] aArg)
-  throws Exception
-  {
-	 Sound.buzz();
-	 Sound.buzz();
-	 Sound.buzz();
-  	 /*
-     RobotState r = RobotState.getInstance();
-     //r.addBehaviour(new DriveForwardAndStopBehaviour());
-     //r.addBehaviour(new EngineTestBehaviour());
-     //r.addBehaviour(new LineFollowBehaviour());
-     r.addBehaviour(new LabyrinthBehaviour());
-     r.init();
-     // r.forward(50);
-     while(!Button.ESCAPE.isPressed()) { 
-    	 r.update();
-     }
-     */
-  }
+import basis.RobotState;
+import behaviour.*;
+import lejos.nxt.*;
+
+public class HelloWorld {
+	public static void main (String[] aArg) throws Exception {
+		RobotState r = RobotState.getInstance();
+		//r.addBehaviour(new DriveForwardAndStopBehaviour());
+		//r.addBehaviour(new EngineTestBehaviour());
+		r.addBehaviour(new LabyrinthBehaviour());
+		r.init();
+	
+		// r.driveForward(10);
+	}
 }
