90257ed8046bf8e0208e3e3a1f9a5823b86a1080
Marek Wehmer <marek.wehmer@googlemail.com>
2011-12-10 14:41:12 +0100

    fixed merge

--- src/test/LabyrinthTest.java
+++ src/test/LabyrinthTest.java
@@ -9,14 +9,7 @@ public class LabyrinthTest {
 		RobotState r = RobotState.getInstance();
 		//r.addBehaviour(new DriveForwardAndStopBehaviour());
 		//r.addBehaviour(new EngineTestBehaviour());
-<<<<<<< HEAD
 		r.addBehaviour(new LineFollowBehaviour());
-=======
-		
-		r.addBehaviour(new LabyrinthBlockingBehaviour());
-		//r.addBehaviour(new WallFollowBehaviour(10));
-
->>>>>>> 5947ba551158c01f08e61b814171f1653cbb8c50
 		r.init();
 		
 		while(true) {
