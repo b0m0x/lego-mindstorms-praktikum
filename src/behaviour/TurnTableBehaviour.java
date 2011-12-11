fcd82afdf52a418e8206cb0b6680f50cd77a10bd
Carsten Griesheimer <sun_od@hotmail.com>
2011-12-11 20:22:38 +0100

    Grundgerüst Drehscheibe

--- src/behaviour/TurnTableBehaviour.java
+++ src/behaviour/TurnTableBehaviour.java
@@ -1,17 +1,41 @@
 package behaviour;
 
+import lejos.nxt.Sound;
+import helper.H;
 import basis.RobotState;
 
 public class TurnTableBehaviour implements RobotBehaviour {
 
+	private RobotState robot;
+	private final int LIGHT = 60;
+	private boolean fertig = false;
+	private LineFollowBehaviour liniensucher;
+	
 	public void init(RobotState r) {
-		// TODO Auto-generated method stub
-
+		robot = r;
+		
 	}
 
 	public void update(RobotState r) {
-		// TODO Auto-generated method stub
-
+		if ( isLight() ) {
+			robot.halt();
+			robot.backwardBlocking(50, 500);
+			robot.rotate(180);
+			robot.backwardBlocking(100, 1000);
+			robot.halt();
+			H.sleep(2000);
+			robot.forwardBlocking(100, 1000);
+			fertig = true;
+		} else {
+			
+			// TODO Liniensuche
+			// if (fertig)
+		}
+	}
+	
+	private boolean isLight() {
+		int color = robot.getLightSensor();
+		return LIGHT <= color;
 	}
 
 }
