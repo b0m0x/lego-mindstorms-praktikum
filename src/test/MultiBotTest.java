773e00d318c2ae1dbbedc287a31a83db2af9ae4d
David Dao <david-dao@gmx.de>
2011-12-12 14:46:32 +0100

    optimized behaviour

--- src/test/MultiBotTest.java
+++ src/test/MultiBotTest.java
@@ -1,30 +1,22 @@
 package test;
 
+import lejos.nxt.Button;
 import basis.*;
+import behaviour.MultiBotBehaviour;
+import behaviour.MultiBotTestBehaviour;
 
 public class MultiBotTest {
 	
 	
 	public static void main(String[] args) {
 		RobotState r = RobotState.getInstance();
-		MultiBotStrategy m = new MultiBotStrategy();
+		r.addBehaviour(new MultiBotBehaviour(3));
+		r.init();
 		
-		while(true) {
-		if(r.crashedIntoWall()) {
-			System.out.println(m.crashedIntoBot);
-			m.setCrashedIntoBot();
-			}
-		
-		if(!r.isMoving()) {
-		r.forward(20);
-		m.setStrategy(3);
-			}
-		
-		if(m.crashedIntoBot) {
-			m.policy(r);
-			m.crashedIntoBot = false;
+		while(true) { 
+			r.update();
 			}
 		}
-	}
+	
 	
 }
