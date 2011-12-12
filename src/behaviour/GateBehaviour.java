792259e78842a09e7eff96932bd72814fa2a2742
David Dao <david-dao@gmx.de>
2011-12-12 17:11:57 +0100

    Bluetooth classes

--- src/behaviour/GateBehaviour.java
+++ src/behaviour/GateBehaviour.java
@@ -1,16 +1,42 @@
 package behaviour;
 
+import basis.Config;
+import basis.GateCommon;
+import basis.GateControl;
 import basis.RobotState;
 
 public class GateBehaviour implements RobotBehaviour {
+	
+	boolean signal_send;
+	GateControl gateControl;
 
+	//noch ohne einreihen
 	public void init(RobotState r) {
-		// TODO Auto-generated method stub
-
+		r.halt();
+		signal_send = false;
+		gateControl = new GateControl();
+		
 	}
 
 	public void update(RobotState r) {
-		// TODO Auto-generated method stub
+		if (!signal_send && Config.currentBehaviour == 6) {
+			while (!gateControl.connectionToGateSuccessful(GateCommon.GATE_1));
+			gateControl.openGate();
+			gateControl.disconnectFromGate();
+			
+			System.out.println("Gate 1 opened");
+			r.forward(30);
+			signal_send = true;
+		}
+		else if (!signal_send && Config.currentBehaviour == 12 ) {
+			while (!gateControl.connectionToGateSuccessful(GateCommon.GATE_2));
+			gateControl.openGate();
+			gateControl.disconnectFromGate();
+			
+			System.out.println("Gate 2 opened");
+			r.forward(30);
+			signal_send = true;
+		}
 
 	}
 
