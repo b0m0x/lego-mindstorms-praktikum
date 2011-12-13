481f808348ab2ec0f9576fed7487bf6d86224738
Carsten Griesheimer <sun_od@hotmail.com>
2011-12-13 04:04:09 +0100

    Endboss

--- src/behaviour/EndBossBehaviour.java
+++ src/behaviour/EndBossBehaviour.java
@@ -1,19 +1,27 @@
 package behaviour;
 
+import helper.Eieruhr;
 import basis.RobotState;
 
 public class EndBossBehaviour implements RobotBehaviour {
-
+	
+	Eieruhr timer = new Eieruhr(120000);
+	private boolean first = true;
+	WallFollowBehaviour wallfollower = new WallFollowBehaviour(8);
+	XMASSearchBehaviour xmass = new XMASSearchBehaviour();
+	
 	public void init(RobotState r) {
-		// TODO Auto-generated method stub
-
+		wallfollower.init(r);
+		xmass.init(r);
 	}
 
 	public void update(RobotState r) {
-		while(!r.isMoving()) {
-			r.forward(100);
+		if (first) timer.reset();
+		if ( !timer.isFinished() ) {
+			wallfollower.update(r);
+		} else {
+			xmass.update(r);
 		}
-
 	}
 
 }
