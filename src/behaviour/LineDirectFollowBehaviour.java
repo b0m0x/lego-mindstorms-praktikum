0144642a2be6c9000ce65b59f39ee9abc924bfd9
Carsten Griesheimer <sun_od@hotmail.com>
2011-12-13 01:27:22 +0100

    ...

--- src/behaviour/LineDirectFollowBehaviour.java
+++ src/behaviour/LineDirectFollowBehaviour.java
@@ -23,13 +23,8 @@ public class LineDirectFollowBehaviour implements RobotBehaviour {
 	private boolean armMovingRight;
 	private final int MIN = -180; // right
 	private final int MAX = -5; // left
-	private boolean searching = true;
-	
-	private final int MAX_SPEED = 30;
-	private final int MIN_SPEED = 25;
+
 	private final NXTRegulatedMotor ARM = Config.SENSOR_MOTOR;
-	private boolean start = true;
-	private final float MIDDLE_ARM_POS = 0.55f;
 	
 	private boolean armHasLine = false;
 	private float ARM_RIGHT_BOUND = 1f;
@@ -37,11 +32,10 @@ public class LineDirectFollowBehaviour implements RobotBehaviour {
 	
 	public void init(RobotState r) {
 		robot = r;		
-		//missLine();
 		ARM.resetTachoCount();
 		ARM.setSpeed( 50 );
 		ARM.forward();
-		r.forward(15);
+		robot.forward(15);
 	}
 	
 	private void keepArmOnLine() {
@@ -65,111 +59,15 @@ public class LineDirectFollowBehaviour implements RobotBehaviour {
 	
 	
 	public void update(RobotState r) {
-		/*if ( start ) {
-			start(); 
-			return;
-		}*/
-		
 		armSchwenkung();
 		keepArmOnLine();
 		isLine(); // onLine
 		robot.bend(1.5f * ((ARM_LEFT_BOUND + ARM_RIGHT_BOUND) / 2 - 0.5f));
-		/*
-		if ( true || !memory.isFinished() || onLine ) {
-			if ( onLine ) {
-			} else {
-				this.armToDefaultBlocking();
-				if (isLineRight) {
-					robot.bend(0.5f);
-				} else {
-					robot.bend(-0.5f);
-				}
-			}
-		} else {
-			missLine();
-		}
-		*/
-		
 	}
 	
-	private void start() {
-		ARM.stop();
-		adjustRobotAndArm();
-		start = false;
-	}
-	
-	private void adjustRobotAndArm() {
-		int direction = armToDefaultBlocking();
-		if (direction == 0) return;
-		
-		robot.forward(30);
-		if (direction == 0) {
-			H.sleep(500);
-		} else if (direction == -1) {
-			// move left
-			robot.bend(0.7f);
-			while ( !isLine() ) {}
-			robot.halt();
-		} else {
-			// move right
-			robot.bend(-0.7f);
-			while ( !isLine() ) {}
-			robot.halt();
-		}
-	}
-	
-	/**
-	 * @return -1 if arm turned left, 0 if no turn, 1 if right turn
-	 */
-	private int armToDefaultBlocking() {
-		int direction = 0;
-		float pos = getRelativeArmPosition();
-		
-		if (pos == MIDDLE_ARM_POS) return 0;
-		if (pos < MIDDLE_ARM_POS){
-			H.p("left");
-			direction = -1;
-			ARM.backward();
-			while (getRelativeArmPosition() < MIDDLE_ARM_POS) {}
-		} else {
-			direction = 1;
-			ARM.forward();
-			while (getRelativeArmPosition() > MIDDLE_ARM_POS) {}
-		}
-		ARM.stop();
-		
-		return direction;
-	}
-	
-
-
-	private Eieruhr noLineTimeout = new Eieruhr(120);
-	
-	private void littleSearch() {
-		if (!noLineTimeout.isFinished()) return;
-		
-//		switch (motorDirection) {
-//		case 0:
-//			ARM.forward();
-//			motorDirection = -1;
-//			noLineTimeout.reset();
-//			break;
-//		case -1:
-//			ARM.backward();
-//			motorDirection = 1;
-//			noLineTimeout.reset();
-//			break;
-//		case 1:
-//			//
-//		}
-	}
 
 	private void missLine() {
 		Sound.beep();
-//		robot.halt();
-//		robot.backward(50);
-//		while ( !isLine() ) {}
-//		memory.reset();
 		// TODO erweitern
 	}
 	
@@ -185,18 +83,6 @@ public class LineDirectFollowBehaviour implements RobotBehaviour {
 		} else if(tacho >= MAX) {
 			ARM.backward();
 			armMovingRight = true;
-		}
-	}
-	
-	/**
-	 * Hier wird die Bewegungsrichtung des Roboters dem Verlauf der Linie angepasst
-	 */
-	private void adjustPath() {
-		float rpos = getRelativeArmPosition();
-		if (rpos < MIDDLE_ARM_POS) {
-			robot.bend(-0.45f); //left
-		} if (rpos > MIDDLE_ARM_POS) {
-			robot.bend(0.6f); //right
 		}
 	}
 	
