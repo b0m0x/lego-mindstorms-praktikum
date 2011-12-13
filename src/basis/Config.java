14b6c1ad1287bad3e8eb1a840a27ecdb4233bd45
Marek Wehmer <marek.wehmer@googlemail.com>
2011-12-13 11:57:13 +0100

    changed color values

--- src/basis/Config.java
+++ src/basis/Config.java
@@ -18,8 +18,8 @@ import lejos.nxt.NXTRegulatedMotor;
 import lejos.nxt.SensorPort;
 
 public final class Config {
-	public final static int COLOR_BRIGHT = 40;
-	public final static int COLOR_BLACK = 30;
+	public final static int COLOR_BRIGHT = 37;
+	public final static int COLOR_BLACK = 28;
 	
 	public final static NXTRegulatedMotor SENSOR_MOTOR = Motor.B;
 	public final static NXTRegulatedMotor LEFT_MOTOR = Motor.C;
